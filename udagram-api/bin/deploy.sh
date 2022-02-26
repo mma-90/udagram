fc475ab5423908d3bb7388bfd58669ebf5ed1f3f
Mohamed Magdy <mmlokas90>
2022-02-26 10:24:02 +0200

    disable hardcode env in api script

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -12,7 +12,7 @@ cd www
 echo ${CIRCLE_BRANCH}
 eb init udagram-app --region $AWS_REGION --platform node.js
 eb use $CIRCLE_BRANCH-udagram-app
-eb setenv POSTGRES_USERNAME="postgres" POSTGRES_PASSWORD="password123" POSTGRES_DB="udagram" PORT="5432" POSTGRES_HOST="udagram-app.cmgb8ilh0484.us-east-1.rds.amazonaws.com" AWS_REGION="us-east-1" AWS_PROFILE="AKIAX6L7YWEGODZ4XIIA" AWS_BUCKET="udagram-web" URL="http://main-udagram-app.eba-4fpa4vv4.us-east-1.elasticbeanstalk.com" JWT_SECRET="IAMJWTSECRETSTRING" 
+# eb setenv POSTGRES_USERNAME="postgres" POSTGRES_PASSWORD="password123" POSTGRES_DB="udagram" PORT="5432" POSTGRES_HOST="udagram-app.cmgb8ilh0484.us-east-1.rds.amazonaws.com" AWS_REGION="us-east-1" AWS_PROFILE="AKIAX6L7YWEGODZ4XIIA" AWS_BUCKET="udagram-web" URL="http://main-udagram-app.eba-4fpa4vv4.us-east-1.elasticbeanstalk.com" JWT_SECRET="IAMJWTSECRETSTRING" 
 # eb setenv
 eb deploy
 eb status 
