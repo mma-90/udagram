15108afc5d26addc90d18207e58cdfa919196419
Mohamed Magdy <mmlokas90>
2022-02-27 07:17:15 +0200

    updating backend script, setting env dynamicly

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,8 +1,17 @@
 cd www
-echo ${CIRCLE_BRANCH}
-eb init udagram-app --region $AWS_REGION --platform node.js
+
+#initialization 
+eb init $APP_NAME --region $AWS_REGION --platform $APP_PLATFORM
+
+#Seting specified environment as default
 eb use $CIRCLE_BRANCH-udagram-app
 
+# Setting env
+eb setenv PORT=$PORT AWS_BUCKET=$AWS_BUCKET AWS_PROFILE=$AWS_PROFILE AWS_REGION=$AWS_REGION JWT_SECRET=$JWT_SECRET POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME URL=$URL
+
+# Deploying
 eb deploy
+
+# checking server status and health
 eb status 
 eb health
